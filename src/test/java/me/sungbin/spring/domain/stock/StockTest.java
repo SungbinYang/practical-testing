af52c5a4b6a560e4b81a97d317e0ec97d33a37b7
SungbinYang <ysb05222@naver.com>
2024-10-20 09:11:47 +0900

    테스트 간 독립성을 보장하자

--- src/test/java/me/sungbin/spring/domain/stock/StockTest.java
+++ src/test/java/me/sungbin/spring/domain/stock/StockTest.java
@@ -8,11 +8,12 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StockTest {
 
+    public static final Stock stock = Stock.create("001", 1);
+
     @Test
     @DisplayName("재고의 수량이 제공된 수량보다 작은지 확인한다.")
     void isQuantityLessThan() {
         // given
-        Stock stock = Stock.create("001", 1);
         int quantity = 2;
 
         // when
@@ -26,7 +27,6 @@ class StockTest {
     @DisplayName("재고를 주어진 개수만큼 차감할 수 있다.")
     void deductQuantity() {
         // given
-        Stock stock = Stock.create("001", 1);
         int quantity = 1;
 
         // when
@@ -40,7 +40,6 @@ class StockTest {
     @DisplayName("재고보다 많은 수의 수량으로 차감 시도 하는 경우 예외가 발생한다.")
     void deductQuantity2() {
         // given
-        Stock stock = Stock.create("001", 1);
         int quantity = 2;
 
         // when & then
